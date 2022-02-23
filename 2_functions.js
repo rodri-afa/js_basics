49c22b67233f30a0e3c1e167562e813a107b057f
rodri-afa <rodrigo.afaformacion@gmail.com>
2022-02-23 09:44:02 +0100

    Merge branch 'main' of https://github.com/rodri-afa/js_basics into main

---
+++ 2_functions.js
@@ -0,0 +1,15 @@
+console.log("2.functions: 👋 Bienvenidos, en este fichero se harán ejercicios sobre las funciones en JS");
+
+//Función saludar1, que muestre "Hola, " y un nombre
+
+//Función saludar2 que devuelva "Hola, " y un nombre
+
+//Función saludar3 igual que la anterior, pero que, en caso de no recibir nombre, devolver "Hola, anónimo"
+
+//Escribir la función saludar2 con la sintaxis de las funciones flecha
+
+//Función que reciba un array y lo devuelva en orden inverso
+
+//Función que cifre un mensaje utilizando el cifrado César con el algoritmo ROT13
+
+//Función que reciba dos números y los mezcle. Ej. mezclar (12,34) => //devuelve 1324
